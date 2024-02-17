37fbdaa4253ce029c84949bae705dea95c572a6a
Tizedit <comunicacion@tizedit.com>
2024-02-17 19:30:54 +0100

    change makeCall func

--- js/fetch.js
+++ js/fetch.js
@@ -2,24 +2,23 @@
 function makeCall() {
 
     // create object with params
-     const params = {
+    const params = {
         param1: 'value1',
         param2: 'value2',
     };
-   
-    fetch('php/service.php', {
+
+    const options = {
         method: 'POST',
-        body: JSON.stringify(params),
+        body: JSON.stringify(params)/* ,
         headers: {
             'Content-type': 'application/json; charset=UTF-8'
-        }
-    }).then(function (response) {
-        return response.json();
-    }).then(function (value) {
-        document.getElementById("res").innerHTML = runObject(value);
-    }).catch(function (err) {
-        console.warn('Error: ', err);
-    });
+        } */
+    }
+   
+    fetch('php/service.php', options)
+        .then(response => response.json())
+        .then(value => document.getElementById("res").innerHTML = runObject(value))
+        .catch(err => console.warn('Error: ', err));
 
 }
 
